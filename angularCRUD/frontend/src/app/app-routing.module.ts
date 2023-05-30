1d69d105686d06bf27a0b409d103f68a58776758
Alin Marin <alinmarin@Alins-MacBook-Pro.local>
2023-05-30 20:32:08 +0300

    curs2

---
+++ angularCRUD/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  { path: 'prima-pagina', component: HomeComponent },
+  { path: 'myadmin', component: AdminComponent },
+  { path: '**', component: HomeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
